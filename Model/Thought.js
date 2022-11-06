72115100e07c0e2293f01897167d435b3d1576e5
Dustin Davisson <dustin.davisson@gmail.com>
2022-11-06 13:38:51 -0800

    Added Thought, leveraging Reaction.

---
+++ Model/Thought.js
@@ -0,0 +1,36 @@
+const { Schema, model } = require('mongoose');
+const reactionSchema = require('./Reaction');
+const { formattedDate } = require('./helper');
+
+const thoughtSchema = new Schema(
+  {
+  thoughtText: {
+    type: String,
+    required: true,
+    min: 1,
+    max: 280,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    get: formattedDate,
+  },
+  username: {
+    type: String,
+    required: true,
+    ref: 'User',
+  },
+  reactions: [
+    reactionSchema
+  ]
+  }
+);
+
+thoughtSchema.virtual('reactionCount')
+  .get(function () {
+    return this.reactions.length;
+  });
+
+const Thought = model('Thought', thoughtSchema);
+
+module.exports = Thought;
