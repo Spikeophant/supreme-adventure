7974c2af9a83c1621610dbd7c23b4bb2cef54cde
Dustin Davisson <dustin.davisson@gmail.com>
2022-11-06 17:21:37 -0800

    Thought routes.

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,3 +1,9 @@
 const router = require('express').Router();
 
 const { createThought, getThought, getThoughts, delThought, updateThought } = require('../../controllers/thoughtController')
+
+router.route('/').get(getThoughts).post(createThought);
+
+router.route('/:id').get(getThought).put(updateThought).delete(delThought);
+
+module.exports = router;
